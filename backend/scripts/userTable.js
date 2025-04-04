d935aa94d6beb0de3f019b608459ed6a4fd0d531
leuzaoo <leuzaoocontato@gmail.com>
2025-04-04 11:00:03 -0300

    Created users table and added new libs to authentication

---
+++ backend/scripts/userTable.js
@@ -0,0 +1,19 @@
+import pool from "../config/db.js";
+
+await pool.query(
+  `
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100),
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW()
+    );
+  `
+);
+
+console.log("Users table created successfully");
+
+await pool.end();
+console.log("Database connection closed after created users table");
+process.exit();
