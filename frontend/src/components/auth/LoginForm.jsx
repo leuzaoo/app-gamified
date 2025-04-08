df69497c64b176a3b5eea76123d116afa49cc38e
Leonardo Costa <94720093+leuzaoo@users.noreply.github.com>
2025-04-08 09:20:22 -0300

    Update frontend/src/components/auth/LoginForm.jsx

--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import { useAuthStore } from "../../store/auth.store";
 
 const LoginForm = () => {
