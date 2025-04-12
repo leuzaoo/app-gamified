cf983fbc0aef5529c26465bc4863224040bcf0a9
leuzaoo <leuzaoocontato@gmail.com>
2025-04-11 21:57:29 -0300

    Added daily quests components

--- frontend/src/components/ui/DailyQuests.jsx
+++ frontend/src/components/ui/DailyQuests.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 import CountdownTimer from "../common/CountdownTimer";
 
+const mockMissions = [
+  { id: 1, title: "Flexões", progress: 0, goal: 10, unit: "" },
+  { id: 2, title: "Agachamentos", progress: 0, goal: 10, unit: "" },
+  { id: 3, title: "Abdominais", progress: 0, goal: 10, unit: "" },
+  { id: 4, title: "Corrida", progress: 0, goal: 5, unit: "km" },
+];
+
 const DailyQuests = () => {
   return (
     <section className="p-4">
       <h1 className="max-w-max border-b text-2xl uppercase">Missões Diárias</h1>
       <ul className="mt-4 space-y-4 font-light">
-        <div className="flex items-center justify-between">
-          <li>Flexões</li>
-          <span>[0/10]</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <li>Agachamentos</li>
-          <span>[0/10]</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <li>Abdominais</li>
-          <span>[0/10]</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <li>Corrida</li>
-          <span>[0/5km]</span>
-        </div>
+        {mockMissions.map((mission) => (
+          <div key={mission.id} className="flex items-center justify-between">
+            <li>{mission.title}</li>
+            <span>
+              [{mission.progress}/{mission.goal}
+              {mission.unit}]
+            </span>
+          </div>
+        ))}
       </ul>
 
       <CountdownTimer />
