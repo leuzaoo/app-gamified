4335b0980c4e0cecf85353414aff5899f0fcee33
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-05-09 10:44:46 -0300

    Created new component

---
+++ frontend/src/components/ui/HorizontalLine.jsx
@@ -0,0 +1,5 @@
+const HorizontalLine = () => {
+  return <hr className="mx-4 mt-4 border-t opacity-20" />;
+};
+
+export default HorizontalLine;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import React from "react";
 
 import { Character, HomeBg, Logo } from "../image-datas/imageLinks";
 
